6347b4de2a6ad374eeeb1ce6cad9fa646cc21dd5
Francisco Oyarzun <foyarzun@OSU.PYTHEAS>
2026-03-09 16:09:46 +0100

    Main update

--- params_exp.py
+++ params_exp.py
@@ -8,57 +8,67 @@ Created on Thu Jan  2 14:25:13 2025
 
 """
 import numpy as np
-#%% Set general parameter
+
+# %% Set general parameter
 
 
 ## WFS and Telescope parameters
 WFSParams = dict(
-{ "Nres": 78, #78,                                                                      # Number of pixels in the aperture of the telescope
-"sampling" : 240/78 ,#240/78,                                                                    # Zero-padding factor (2 is Shannon)
-"D" : 1.52 ,                                                                        # Telescope diameter (m)
-"Nphotons" : [4, 6],                                                               # Log range of number of photons in measurement    
-"RON" : [0.6, 0.6]     ,                                                                # Read-out noise in photons per pixel per frame
-"Nzernike" : 50 ,                                                                  # Number of Zernike modes to reconstruct
-"Nactuator" : 17,                                                                  # Number of actuators across the diameter of the DM
-"useNoise" : True,
-"InitParam" : [1.2, 1.2, 1000],
-"MaskType" : "BiOEdge",
-"Reconstruction": "SimpleNet",
-"FrameBufferLength": 1,
-"beamSplitProportionForWFSDetector": 1.,
-"ModalBasis" : "Zernike"
-}
-)  
+    {
+        "Nres": 78,  # 40,                                                              # Number of pixels in the aperture of the telescope
+        "sampling": 240
+        / 78,  # 6,                                                        # Zero-padding factor (2 is Shannon)
+        "D": 1.52,  # Telescope diameter (m)
+        "Modulation": 0,  # Modulation in \lambda/D
+        "Nphotons": [4.5, 6],  # Log range of number of photons in measurement
+        "RON": [1, 2],  # Read-out noise in photons per pixel per frame
+        "Nzernike": 150,  # Number of modes to reconstruct
+        "Nactuator": 17,  # Number of actuators across the diameter of the DM
+        "useNoise": True,  # use photon and read-out noise
+        "InitParam": [1.57, 1.57],  # wavefront sensor specific parameters
+        "MaskType": "Papyrus",  # Type of wfs. The full list can be found in Constants.py in mask_types_list
+        "Reconstruction": "Papyrus",  # Reconstruction algorithm. The full list can be found in Constants.py in reconstruction_types_list
+        "beamSplitProportionForWFSDetector": 1.0,  # Add the effects of a beam splitter to assign the correct amount of light to the wfs frame and psf frame
+        "ModalBasis": "Papyrus_KL",  # Basis used for the reconstruction. The full list can be found in Constants.py in basis_list
+        "Substract_Reference": True,  # Substract or not the reference intensity frame
+        "Extract_pupils": True,
+        "Bin_factor": 1,
+        "Center_noise": 2,
+        "Use_MTF": True,
+        "MTF_upscale": 10,
+    }
+)
 
-                                                               
+
 ## Atmosphere parameters
 AtmosParams = dict(
-    {                                                                   
-"r0" : [0.05, 0.2],           
-"L0" : [20,30.0000],                                                                    # Inner scale (m)                                                                       # Outter scale (m)
-"Nphases" : 16,
-"Layers" : [1,2]
-}
-)                             
-
+    {
+        "r0": [0.05, 0.2],  # Fired parameter range (m)
+        "L0": [20, 30.0000],  # Outter scale range (m)
+        "Nphases": 16,  # Number of phases in the batch
+        "Layers": [3, 5],  # Number of layers in phase range
+        "f_slope": 11.0 / 6.0,  # Slope of the spectrum (11/6) is the default
+    }
+)
 
 
 ## Loop parameters
 LoopParams = dict(
-{
-"loopFrequency" : 1000,
-"delayFrames" : 1,
-"windSpeedVector" : [-10,10],
-"levelOfCorrection" : [0., 1.],
-}
+    {
+        "loopFrequency": 1000,
+        "delayFrames": 1,
+        "windSpeedVector": [-10, 10],
+        "levelOfCorrection": [0.0, 1.0],
+    }
 )
 
 ## Train parameters
 TrainParams = dict(
-{
-"lro" : 1e-3,
-"lrn" : 1e-3,
-"TrainRunNb" : 5000,
-"TestRunNb" : 10,
-}
+    {
+        "lro": 2e-4,
+        "lrn": 1e-4,
+        "TrainRunNb": 5000,
+        "TestRunNb": 10,
+        "OptimizeMask": False,
+    }
 )
