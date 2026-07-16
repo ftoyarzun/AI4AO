f4b5305f0446b7e1d80917deadee41ca86ff6bdc
ftoyarzun <ftoyarzun@uc.cl>
2026-07-16 14:02:35 +0200

    Work towards proper package

---
+++ AI4AO/__init__.py
@@ -0,0 +1,5 @@
+from .TorchPropagator import WFS
+from .MaskGeneration import MaskManager
+from .PhaseDataset import PhaseDataset
+from .DeformableMirror import DeformableMirror
+from .FramePreprocess import FramePreprocess
